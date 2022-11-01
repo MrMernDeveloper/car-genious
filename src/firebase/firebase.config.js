9a459aa4a35281d5a73921b67f84c8fabac75845
MrMernDeveloper <rakibkst16@gmail.com>
2022-11-01 22:11:33 +0600

    first commit

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,15 +5,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
+  apiKey: "AIzaSyAfuEGCTbdGQyAp56-2hshlj08qq4MVaAw",
+  authDomain: "car-genious-6e46b.firebaseapp.com",
+  projectId: "car-genious-6e46b",
+  storageBucket: "car-genious-6e46b.appspot.com",
+  messagingSenderId: "858973276232",
+  appId: "1:858973276232:web:498be783af3be80fef307d"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export default app;
